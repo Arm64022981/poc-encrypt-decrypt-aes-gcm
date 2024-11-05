480a41271a101302692bd8406f102e91679cd747
Mouch64022 <64022879@up.ac.th>
2024-11-05 18:00:05 +0700

    Function EncryptDecrypt

--- src/dto/response/encrypt-decrypt.tsx
+++ src/dto/response/encrypt-decrypt.tsx
@@ -1,4 +1,3 @@
-
 export interface EncryptDecryptModel {
      plaintext: string;
   }
--- src/service/encrypt-decrypt/decrypt.tsx
+++ src/service/encrypt-decrypt/decrypt.tsx
@@ -1,26 +1,23 @@
+// controller
 'use server';
 import {EncryptDecryptRequest} from '@/dto/request/encrypt-decrypt'
 import {EncryptDecryptModel} from '@/dto/response/encrypt-decrypt'
-import crypto from 'crypto';
-
-export default function decryptMessage(param: EncryptDecryptRequest, res: EncryptDecryptModel) {
- 
-
-  try {
-      const data = Buffer.from(param, 'base64');
-      const plaintext = data.slice(0, 12);
+// import {cipherText} from '@/service/encrypt-decrypt/enctypt'
+import crypto from 'crypto'
+const AES_KEY = 'hzZ6rE1kGiJWQwCO';
+export default async function decryptMessage(param: EncryptDecryptRequest): Promise<EncryptDecryptModel>{
+    try {
+      const data = Buffer.from(param.plaintext, 'base64');
+      const iv = data.slice(0, 16);
       const tag = data.slice(data.length - 16);
-      const encrypted = data.slice(12, data.length - 16);
-
-      const decipher = crypto.createDecipheriv('aes-128-gcm', Buffer.from(key), plaintext);
-      decipher.setAuthTag(tag);
-
-      const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]);
-      res.status(200).json({ plaintext: decrypted.toString('utf8') });
-    } catch (error) {
-      res.status(500).json({ error: 'Decryption failed' });
-    }
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
-  }
-}
+      const encrypted = data.slice(16, data.length - 16);
+      const decipher = crypto.createDecipheriv('aes-128-gcm',AES_KEY, iv);
+      decipher.setAuthTag(tag); 
+      const decrypted = Buffer.concat([decipher.update(encrypted), decipher.final()]).toString('utf-8');
+      return ({
+        plaintext: decrypted   });  
+    }  catch (error) {
+      console.error('Error fetching:', error);      
+    return ({ 
+      plaintext: '' });  
+  }}
